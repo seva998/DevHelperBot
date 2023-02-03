7cb64ed3eeffae388416860628d738e899051c70
seva998 <sevauim5gm@gmail.com>
2023-02-03 15:10:04 +0300

    1

---
+++ src/main/java/Vsevolod/DevHelperBot/DevHelperBotApplication.java
@@ -0,0 +1,13 @@
+package Vsevolod.DevHelperBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevHelperBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevHelperBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Vsevolod/DevHelperBot/DevHelperBotApplicationTests.java
@@ -0,0 +1,13 @@
+package Vsevolod.DevHelperBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHelperBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
